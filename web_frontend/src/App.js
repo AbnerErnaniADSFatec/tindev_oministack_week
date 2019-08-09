9a5a74dc7496429b8400f8162e2ff1cbbfde0c4a
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-08-09 16:37:10 -0300

    01h:05m:05s

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Login from './pages/Login';
+import Routes from './routes';
 
 function App() {
   return (
-    <Login />
+    <Routes />
   );
 }
 
